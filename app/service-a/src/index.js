173be8c56d53619788b1ad4653160efa21034d87
ahmedadel-1 <ahmedadel1591999@gmail.com>
2025-08-07 16:15:54 +0300

    Update greeting message

--- app/service-a/src/index.js
+++ app/service-a/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from GKE + ArgoCD!'));
+app.get('/', (req, res) => res.send('🚀 Hello from GKE + ArgoCD with GitOps!'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
